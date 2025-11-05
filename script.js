8df0c68b88f25df7aa7bddf7d5bdb93c64bf653d
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-05 10:24:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ function abrirModal(img) {
   const modal = document.getElementById("modal");
   const imagemModal = document.getElementById("imagemModal");
   const descricao = document.getElementById("descricao");
-  modal.style.display = "block";
+
   imagemModal.src = img.src;
   descricao.innerText = img.alt;
+
+  modal.classList.add("mostrar");
 }
 
 function fecharModal() {
-  document.getElementById("modal").style.display = "none";
+  const modal = document.getElementById("modal");
+  modal.classList.remove("mostrar");
 }
+
+document.getElementById("modal").addEventListener("click", (e) => {
+  if (e.target.id === "modal") {
+    fecharModal();
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    fecharModal();
+  }
+});
