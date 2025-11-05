820717cf23e9f531721a7d1d8e7c0ad86024a2ab
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-05 11:16:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function abrirModal(img) {
+let indiceAtual = 0;
+const imagens = Array.from(document.querySelectorAll(".galeria img"));
+imagens.forEach((img, i) => img.addEventListener("click", () => abrirModal(i)));
+
+function abrirModal(indice) {
+  indiceAtual = indice;
   const modal = document.getElementById("modal");
   const imagemModal = document.getElementById("imagemModal");
   const descricao = document.getElementById("descricao");
 
-  imagemModal.src = img.src;
-  descricao.innerText = img.alt;
-
   modal.classList.add("mostrar");
+
+  imagemModal.classList.remove("visivel");
+
+  imagemModal.src = imagens[indiceAtual].src;
+  descricao.innerText = imagens[indiceAtual].alt;
+
+  setTimeout(() => {
+    imagemModal.classList.add("visivel");
+  }, 100);
 }
 
 function fecharModal() {
@@ -14,14 +25,33 @@ function fecharModal() {
   modal.classList.remove("mostrar");
 }
 
-document.getElementById("modal").addEventListener("click", (e) => {
-  if (e.target.id === "modal") {
-    fecharModal();
+function navegar(direcao) {
+  indiceAtual += direcao;
+
+  if (indiceAtual < 0) {
+    indiceAtual = imagens.length - 1;
+  } else if (indiceAtual >= imagens.length) {
+    indiceAtual = 0;
   }
+
+  const imagemModal = document.getElementById("imagemModal");
+  const descricao = document.getElementById("descricao");
+
+  imagemModal.classList.remove("visivel");
+
+  setTimeout(() => {
+    imagemModal.src = imagens[indiceAtual].src;
+    descricao.innerText = imagens[indiceAtual].alt;
+    imagemModal.classList.add("visivel");
+  }, 100);
+}
+
+document.getElementById("modal").addEventListener("click", (e) => {
+  if (e.target.id === "modal") fecharModal();
 });
 
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    fecharModal();
-  }
+  if (e.key === "Escape") fecharModal();
+  if (e.key === "ArrowRight") navegar(1);
+  if (e.key === "ArrowLeft") navegar(-1);
 });
